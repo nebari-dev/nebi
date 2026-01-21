70d8111022034e560d8a22c79198aa824a32bbd5
Amit Kumar <aktech@users.noreply.github.com>
2026-01-21 14:57:35 +0000

    working app

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Run Wails application
 	err := wails.Run(&options.App{
-		Title:  "Darb - Environment Manager",
+		Title:  "Nebi - Environment Manager",
 		Width:  1280,
 		Height: 800,
 		AssetServer: &assetserver.Options{
